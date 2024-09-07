ab688ea51787d11ee6dc1ce1c5aae29a58ff54a6
Runar Saur Modahl <runarsm@stud.ntnu.no>
2024-09-07 23:11:57 +0200

    format repo

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TestComponent } from "./TestComponent";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
-
     return (
         <QueryClientProvider client={queryClient}>
-            <TestComponent pokemonId={1}/>
+            <TestComponent pokemonId={1} />
         </QueryClientProvider>
     );
 }
--- src/api/Requests.ts
+++ src/api/Requests.ts
@@ -2,14 +2,14 @@ import axios, { AxiosResponse } from "axios";
 import { PokeAPI } from "pokeapi-types";
 
 export class Requests {
+    static readonly url: string = "https://pokeapi.co/api/v2/";
 
-    static url: string = "https://pokeapi.co/api/v2/";
-
-    static async getPokemonById(id: number) : Promise<PokeAPI.Pokemon>{
-        return axios.get(this.url + "pokemon/" + id)
+    static async getPokemonById(id: number): Promise<PokeAPI.Pokemon> {
+        return axios
+            .get(this.url + "pokemon/" + id)
             .then((result: AxiosResponse<PokeAPI.Pokemon>) => {
                 console.log(result.data);
                 return result.data;
-        });
+            });
     }
 }
