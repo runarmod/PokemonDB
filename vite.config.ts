50fbe4462448c9984b6eb6ab4e0ce7f4dea2daab
Petter Teisberg <pteisb@gmail.com>
2024-09-17 21:59:23 +0200

    set up test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-});
+    server: {
+      open: true,
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: 'src/test/setup.ts',
+      css: true
+    }
+  });
