d133ca318e0cc8b8f5a556e99ae675365cd95818
Runar Saur Modahl <runarsm@stud.ntnu.no>
2024-09-19 11:37:05 +0200

    fix disappearing images and alignment in sortAndFilter component

--- src/components/SortAndFilter.tsx
+++ src/components/SortAndFilter.tsx
@@ -29,7 +29,7 @@ export default function SortAndFilter() {
                 }
             >
                 <img
-                    src={`${isMenuOpen ? X : SortFilter}?${isMenuOpen}`}
+                    src={isMenuOpen ? X : SortFilter}
                     alt={isMenuOpen ? "Close" : "Sort/filter"}
                 />
             </button>
